d0c7aea1a3f01d64a46665ce2f22c9a1474ee12a
danielthepope <djpope2365@gmail.com>
2014-02-05 21:14:44 +0000

    Displays information from sensors and reacts
    Reacts by reversing when there is something 10cm in front of it, and
    displays information from the light sensor.
    .gitignore ignores the NXT compiled files.
    Also plays a bangin' donk.

--- Boostbot/src/Boost.java
+++ Boostbot/src/Boost.java
@@ -1,11 +1,80 @@
+import java.io.File;
+
 import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
+import lejos.nxt.UltrasonicSensor;
 
 
 public class Boost {
 
-	public static void main(String[] args) {
-		System.out.println("MY NAME IS BOOST!");
-		Button.waitForAnyPress();
+	public static void main(String[] args) throws Exception {
+		UltrasonicSensor sensor = new UltrasonicSensor(SensorPort.S4);
+		LightSensor ls = new LightSensor(SensorPort.S1);
+		ls.setFloodlight(true);
+		Motor.B.setSpeed(90);
+		Motor.C.setSpeed(90);
+		int distance = 255;
+		int brightness = 0;
+		final File donk = new File("donk2.wav"); 
+		Sound.playSample(donk, 100);
+		while(!Button.ESCAPE.isDown()) {
+			LCD.clear();
+			LCD.drawString("I AM BOOST", 0, 0);
+			distance = sensor.getDistance();
+			brightness = ls.readValue();
+			LCD.drawString("distance: " + distance + "cm", 0, 1);
+			LCD.drawString("   light: " + brightness, 0, 2);
+			if (Button.ENTER.isDown()) {
+				forward(1);
+			}
+			if (Button.RIGHT.isDown()) {
+				turnLeft(1);
+			}
+			if (Button.LEFT.isDown()) {
+				turnRight(1);
+			}
+			if (distance < 10) {
+//				Sound.playNote(Sound.PIANO, 880 - (distance * 20), 500);
+				LCD.drawString("MOVE BITCH", 0, 3);
+				LCD.drawString("GET OUT THE WAY!", 0, 4);
+				Motor.B.backward();
+				Motor.C.backward();
+//				Sound.beep();
+			} else {
+				Motor.B.stop(true);
+				Motor.C.stop(true);
+			}
+			Thread.sleep(250);
+		}
+		ls.setFloodlight(false);
+	}
+	
+	public static void forward(float seconds) throws Exception {
+		Motor.B.forward();
+		Motor.C.forward();
+		Thread.sleep((long) (1000*seconds));
+		Motor.B.stop(true);
+		Motor.C.stop(true);
+	}
+	
+	public static void turnRight(float seconds) throws Exception {
+		Motor.B.backward();
+		Motor.C.forward();
+		Thread.sleep((long) (1000*seconds));
+		Motor.B.stop(true);
+		Motor.C.stop(true);
+	}
+	
+	public static void turnLeft(float seconds) throws Exception {
+		Motor.B.forward();
+		Motor.C.backward();
+		Thread.sleep((long) (1000*seconds));
+		Motor.B.stop(true);
+		Motor.C.stop(true);
 	}
 
 }
