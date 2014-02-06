845b5cd044a60a36183b0bd58d97b311e6a76b71
danielthepope <djpope2365@gmail.com>
2014-02-06 11:19:53 +0000

    Checks if there is a wall on the side
    Also added a method to rotate in degrees

--- Boostbot/src/Boost.java
+++ Boostbot/src/Boost.java
@@ -11,40 +11,47 @@ import lejos.nxt.UltrasonicSensor;
 
 public class Boost {
 
+	static UltrasonicSensor sensor;
+	static LightSensor ls;
 	public static void main(String[] args) throws Exception {
-		UltrasonicSensor sensor = new UltrasonicSensor(SensorPort.S4);
-		LightSensor ls = new LightSensor(SensorPort.S1);
+		sensor = new UltrasonicSensor(SensorPort.S4);
+		ls = new LightSensor(SensorPort.S1);
 		ls.setFloodlight(true);
 		Motor.B.setSpeed(90);
 		Motor.C.setSpeed(90);
 		int distance = 255;
 		int brightness = 0;
-		final File donk = new File("donk2.wav"); 
-		Sound.playSample(donk, 100);
+//		final File donk = new File("donk2.wav"); 
+//		Sound.playSample(donk, 100);
 		while(!Button.ESCAPE.isDown()) {
-			LCD.clear();
+//			LCD.clear();
 			LCD.drawString("I AM BOOST", 0, 0);
 			distance = sensor.getDistance();
 			brightness = ls.readValue();
+			LCD.clear(1);
 			LCD.drawString("distance: " + distance + "cm", 0, 1);
+			LCD.clear(2);
 			LCD.drawString("   light: " + brightness, 0, 2);
+			isThereAWall();
 			if (Button.ENTER.isDown()) {
 				forward(1);
 			}
 			if (Button.RIGHT.isDown()) {
-				turnLeft(1);
+				turnLeft(90);
 			}
 			if (Button.LEFT.isDown()) {
-				turnRight(1);
+				turnRight(90);
 			}
 			if (distance < 10) {
 //				Sound.playNote(Sound.PIANO, 880 - (distance * 20), 500);
 				LCD.drawString("MOVE BITCH", 0, 3);
 				LCD.drawString("GET OUT THE WAY!", 0, 4);
-				Motor.B.backward();
-				Motor.C.backward();
+				// Motor.B.backward();
+				// Motor.C.backward();
 //				Sound.beep();
 			} else {
+				LCD.clear(3);
+				LCD.clear(4);
 				Motor.B.stop(true);
 				Motor.C.stop(true);
 			}
@@ -53,28 +60,59 @@ public class Boost {
 		ls.setFloodlight(false);
 	}
 	
-	public static void forward(float seconds) throws Exception {
+	public static void forward(double revolutions) throws Exception {
 		Motor.B.forward();
 		Motor.C.forward();
-		Thread.sleep((long) (1000*seconds));
+		Thread.sleep((long) (4000*revolutions));
 		Motor.B.stop(true);
 		Motor.C.stop(true);
 	}
 	
-	public static void turnRight(float seconds) throws Exception {
+	public static void goRight(double revolutions) throws Exception {
 		Motor.B.backward();
 		Motor.C.forward();
-		Thread.sleep((long) (1000*seconds));
+		Thread.sleep((long) (4000*revolutions));
 		Motor.B.stop(true);
 		Motor.C.stop(true);
 	}
 	
-	public static void turnLeft(float seconds) throws Exception {
+	public static void goLeft(double revolutions) throws Exception {
 		Motor.B.forward();
 		Motor.C.backward();
-		Thread.sleep((long) (1000*seconds));
+		Thread.sleep((long) (4000*revolutions));
 		Motor.B.stop(true);
 		Motor.C.stop(true);
 	}
 
+	public static void turnRight(int degrees) {
+		Motor.C.rotate(degrees * 2, true);
+		Motor.B.rotate((0 - degrees) * 2, false);
+	}
+
+	public static void turnLeft(int degrees) {
+		Motor.B.rotate(degrees * 2, true);
+		Motor.C.rotate((0 - degrees) * 2, false);
+	}
+	
+	public static boolean isThereAWall() throws Exception {
+		int onValue, offValue, difference;
+		ls.setFloodlight(true);
+		Thread.sleep(250);
+		onValue = ls.readValue();
+		Thread.sleep(250);
+		ls.setFloodlight(false);
+		Thread.sleep(250);
+		offValue = ls.readValue();
+		difference = onValue - offValue;
+//		Thread.sleep(250);
+		LCD.clear(5);
+		LCD.clear(6);
+		LCD.drawString("diff=" + difference, 0, 6);
+		if (difference > 4) {
+			LCD.drawString("Hello wall!", 0, 5);
+			return true;
+		}
+//		LCD.drawString("on:" + onValue + " off:" + offValue, 0, 5);
+		return false;
+	}
 }
--- README.md
+++ README.md
@@ -2,4 +2,20 @@ BOOST
 =====
 
 CM30229 Coursework 1
-Edit by David
+
+Robot thoughts
+--------------
+
+With the side led, keep flashing it. If there is a significant difference between the reading when the light is on and the reading when the light is off, there is a wall there. 
+
+Step 1: Go forward slowly, sweeping the ultrasound head around a 180 degree angle. The head will eventually get to a point where at a particular angle, a wall will be the correct distance away. 
+
+Step 2: This angle can be calculated. Rotate robot so that it is parallel to the wall. 
+
+Step 3: Flash the led, taking a light measurement with light on and another with the light off. There should be a difference. Keep taking this measurement every half second. 
+
+Step 4: move forwards with the led flashing add and the ultrasound pointing directly ahead. At some point, Either the ultrasound will detect a wall or the difference between light on and light off will be minimal. 
+
+Corner turning
+
+If the wall detector no longer detects a wall, it could be because it has veered away from the wall slowly. It should turn left 45 degrees, then sweep to look for a wall within 20cm. If there is, align up to the wall and carry on. Otherwise it should turn the full 90 degrees (45 more) and carry on.
