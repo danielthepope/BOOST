518dca2e64d8925d8f3cbc37079611998b2e1e57
parida <dp297@bath.ac.uk>
2014-02-09 14:31:14 +0000

    Perpendicular check
    Added code to check if BOOST is perpendicular to a wall, and how to
    handle it.

--- Boostbot/src/Boost.java
+++ Boostbot/src/Boost.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
@@ -13,9 +14,12 @@ public class Boost {
 
 	static UltrasonicSensor sensor;
 	static LightSensor ls;
+	public static final int MIN_DIST = 10; 
+	
 	public static void main(String[] args) throws Exception {
 		sensor = new UltrasonicSensor(SensorPort.S4);
 		ls = new LightSensor(SensorPort.S1);
+		
 		ls.setFloodlight(true);
 		Motor.B.setSpeed(90);
 		Motor.C.setSpeed(90);
@@ -42,7 +46,7 @@ public class Boost {
 			if (Button.LEFT.isDown()) {
 				turnRight(90);
 			}
-			if (distance < 10) {
+			if (distance < MIN_DIST) {
 //				Sound.playNote(Sound.PIANO, 880 - (distance * 20), 500);
 				LCD.drawString("MOVE BITCH", 0, 3);
 				LCD.drawString("GET OUT THE WAY!", 0, 4);
@@ -63,6 +67,14 @@ public class Boost {
 	public static void forward(double revolutions) throws Exception {
 		Motor.B.forward();
 		Motor.C.forward();
+		Thread.sleep((long) (4000*revolutions));
+		Motor.B.stop(true);
+		Motor.C.stop(true);
+	}
+	
+	public static void backward(double revolutions) throws Exception {
+		Motor.B.backward();
+		Motor.C.backward();
 		Thread.sleep((long) (4000*revolutions));
 		Motor.B.stop(true);
 		Motor.C.stop(true);
@@ -114,5 +126,115 @@ public class Boost {
 		}
 //		LCD.drawString("on:" + onValue + " off:" + offValue, 0, 5);
 		return false;
+	}
+	
+	public static void rotateHead(int degrees)
+	{
+		Motor.A.rotate(degrees);
+	}
+	
+	public static ArrayList<Integer> calculateDistances(int rdeg)
+	{
+		ArrayList<Integer> distances = new ArrayList<Integer>();
+		
+		//distance straight ahead
+		distances.add(sensor.getDistance());
+		
+		//rotate positive degree
+		for(int i = 0; i <= 90; i+=rdeg)
+		{
+			rotateHead(rdeg);
+			distances.add(sensor.getDistance());
+		}
+		
+		//return to original position
+		rotateHead(-90);
+		
+		//rotate negative degree
+		for(int i = 0; i <= 90; i+=rdeg)
+		{
+			rotateHead(-rdeg);
+			distances.add(sensor.getDistance());
+		}
+		
+		//return to original position
+		rotateHead(90);
+		
+		return distances;
+	}
+	
+	public static boolean amIPerpendicular(ArrayList<Integer> distances, int rdeg)
+	{
+		//Find the distance with the least value
+		//If there's a tie pick the first one
+		int minimumDistance = 255;
+		int minimumIndex = distances.size();
+				
+		for(int j = 0; j < distances.size(); j++)
+		{
+			if( distances.get(j) < minimumDistance )
+			{
+				minimumDistance = distances.get(j);
+				minimumIndex = j;
+			}
+		}
+		
+		//now we analyse the data
+		if(minimumIndex == 0 
+				|| minimumIndex == distances.size()-1/2 
+				|| minimumIndex == distances.size()-1)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static void findPerpendicularWall(ArrayList<Integer> distances, int rdeg)
+	{	
+		//Find the distance with the least value
+		//If there's a tie pick the first one
+		int minimumDistance = 255;
+		int minimumIndex = distances.size();
+		
+		for(int j = 0; j < distances.size(); j++)
+		{
+			if( distances.get(j) < minimumDistance )
+			{
+				minimumDistance = distances.get(j);
+				minimumIndex = j;
+			}
+		}
+		
+		//now we analyse the data
+		if(minimumDistance <= MIN_DIST) //we are very close to a wall
+		{
+			LCD.clear(3);
+			LCD.clear(4);
+			LCD.drawString("WALL TOO CLOSE", 0, 3);
+			
+			//Go backwards and repeat
+		}
+		else if(minimumDistance < 255)
+		{
+			LCD.clear(3);
+			LCD.clear(4);
+			LCD.drawString("DIST: " + minimumDistance, 0, 3);
+			
+			int angle = minimumIndex * rdeg;
+			
+			if(minimumIndex > distances.size()-1/2)
+			{
+				angle = angle - 90;
+				LCD.drawString("TURNING LEFT: " + angle, 0, 3);
+				turnLeft(angle);
+			}
+			else
+			{
+				LCD.drawString("TURNING RIGHT: " + angle, 0, 3);
+				turnRight(angle);
+			}	
+		}
+		
 	}
 }
